3d63ac8e8bcabccdd8cd36beaf8493a268564fab
kishorekkp <kishorekpani007@gmail.com>
2022-07-29 22:05:45 +0530

    First commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com',
+      'images.app.goo.gl'
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
